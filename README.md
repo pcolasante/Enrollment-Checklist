2884714a574f80018f7e1024f1f202fbb2a30e4f
Paulina Flores <118872813+pcolasante@users.noreply.github.com>
2024-10-20 00:08:31 +0000

    8:07pm
    Added filtering system, needs work.
    Basic js and html stuff as well

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Submission for the TADHack Orlando 2024.
 Steps of Enrollment into Valencia College for prospectives students into the academic programs. 
 Intent is to make it interactive and personalized for the student. 
 
-Student can click on steps as they complete it, and see further information when interacting with it, such as links to Valencia College webpages/resources available to students.  
+Student can click on steps as they complete it, and see further information when interacting with it, such as links to Valencia College webpages/resources available to students. 
+
+Possibly do a filtering system so student can filter the steps by completed, in progress, or not started yet. 
 
 Optional goal is to make a bot assistant that can help/respond to students with general questions of the steps of enrollment.
 Possibly attempt to make it so bot can research student profile or previous interactions to personalize the answer. 
--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 // For testing purposes
 console.log("This thing actually works! Congrats.");
+
+// ************************************************************
+var clearComplete = document.getElementById("clearComplete");
+
+//If we end up creating filtering system
+var showAll = document.getElementById("showAll");
+var showComplete = document.getElementById("showComplete");
+var showInprogress = document.getElementById("showInprogress");
+var showNotStarted = document.getElementById("showNotStarted");
+var showState = "showAll";
+
+
+function changeProgress() {
+    // console.log("hello");
+    var parentCard = this.parentElement;
+    var status = parentCard.classList[1];
+    parentCard.classList.remove(status);
+    var statusElem = parentCard.children[2];
+
+
+    //Filtering system
+    //I don't like this actually lol, it could be way better, let's google that after
+    if (status == "Not-started") {
+      parentCard.classList.add("In-progress");
+      statusElem.innerHTML = "In-progress";
+      statusElem.classList.remove(statusElem.classList[1]);
+      statusElem.classList.add("color-blue");
+    } else if (status == "Completed") {
+      parentCard.classList.add("In-progress");
+      statusElem.innerHTML = "In-progress";
+      statusElem.classList.remove(statusElem.classList[1]);
+      statusElem.classList.add("color-blue");
+    } else {
+      parentCard.classList.add("Completed");
+      statusElem.innerHTML = "Completed";
+      statusElem.classList.remove(statusElem.classList[1]);
+      statusElem.classList.add("color-green");
+    }
+    //What if I do the checklist from HTML and style it on CSS and make my life easier
+    //meh
+  }
